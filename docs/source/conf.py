1148d5ae1425844f4897201a2a51cfa9c7b5a3ea
S Abney <abney@umich.edu>
2022-02-05 17:00:25 -0500

    Cleaned out template material

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Selkie'
+copyright = '2022, University of Michigan'
+author = 'Steven Abney'
 
 release = '0.1'
 version = '0.1.0'
